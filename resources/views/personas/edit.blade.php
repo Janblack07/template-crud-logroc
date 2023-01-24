b264e55caa17a36fbe071fa3792dfbb9f01949d4
Jean Rodríguez <jeanrodriguez060420@gmail.com>
2023-01-23 22:50:51 -0500

    listo

--- resources/views/personas/edit.blade.php
+++ resources/views/personas/edit.blade.php
@@ -1,47 +1,47 @@
 @extends('welcome')
 @section('contenido')
-<h1>Hola desde edit</h1>
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <!-- left column -->
-            <div class="col-md-10 mx-auto">
-                <!-- general form elements -->
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h3 class="card-title">Registro de Persona</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <!-- form start -->
-                    <form action="{{route('persona.update',$personas->id)}}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label >Nombre de la persona</label>
-                                <input type="text" class="form-control"  placeholder="Nombre de la persona" name="nombreP" value="{{$personas->nombreP}}">
-                            </div>
-                            <div class="form-group">
-                                <label >Apellido de la persona</label>
-                                <input type="text" class="form-control"  placeholder="Apellido de la persona" name="apellidoP" value="{{$personas->apellidoP}}">
-                            </div>
-                            <div class="form-group">
-                                <label >Edad de la persona</label>
-                                <input type="number" class="form-control"  placeholder="Edad de la persona" name="edadP" value="{{$personas->edadP}}">
-                            </div>
-                            <div class="form-group">
-                                <label >Cedula de la persona</label>
-                                <input type="text" class="form-control"  placeholder="Cedula de la persona" name="cedulaP" value="{{$personas->cedulaP}}">
-                            </div>
-                            <div >
-                               <button class="btn btn-primary">Actualizar</button>
-                            </div>
+    <h1>Hola desde edit</h1>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-10 mx-auto">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Registro de Persona</h3>
                         </div>
-                        <!-- /.card-body -->
-
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.container-fluid -->
-</section>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="{{ route('persona.update', $personas->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label>Nombre de la persona</label>
+                                    <input type="text" class="form-control" placeholder="Nombre de la persona"
+                                        name="nombreP" value="{{ $personas->nombreP }}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Apellido de la persona</label>
+                                    <input type="text" class="form-control" placeholder="Apellido de la persona"
+                                        name="apellidoP" value="{{ $personas->apellidoP }}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Edad de la persona</label>
+                                    <input type="number" class="form-control" placeholder="Edad de la persona"
+                                        name="edadP" value="{{ $personas->edadP }}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Cedula de la persona</label>
+                                    <input type="text" class="form-control" placeholder="Cedula de la persona"
+                                        name="cedulaP" value="{{ $personas->cedulaP }}">
+                                </div>
+                                <div>
+                                    <button class="btn btn-primary">Actualizar</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+    </section>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__wobble" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   @include('template.nav')
@@ -15,7 +15,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Practica</span>
     </a>
 
